e2ac1007d2b38114ffab7bb294ea752244f641e7
Aditya-Kumar-Gupta <adityakrgupta1609@gmail.com>
2026-09-04 22:35:07 +0530

    feat: add pattern printing scripts (star and number pyramids)

---
+++ Pyramid.py
@@ -0,0 +1,22 @@
+'''
+            Output :
+           
+           * 
+           * * 
+           *   * 
+           *     * 
+           * * * * * 
+'''
+
+
+r = int(input("Enter the number of rows : "))
+
+print("\n\tPyramid Shape :")
+
+for i in range (0,r):
+    for j in range (0,i+1):
+        if (j==0) or (i==r-1) or (i==j) or ():
+            print("*", end=" ")
+        else :
+            print(" ",end=" ")
+    print()
