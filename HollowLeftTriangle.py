218c40af945eb9966e51abd625690fc16d11d086
Aditya-Kumar-Gupta <adityakrgupta1609@gmail.com>
2026-09-05 16:24:34 +0530

    feat: add pattern printing scripts (star and number pyramids)

--- HollowLeftTriangle.py
+++ HollowLeftTriangle.py
@@ -14,7 +14,7 @@ print("\n\tHollow left triangle Shape :")
 
 for i in range (0,r):
     for j in range (0,i+1):
-        if (j==0) or (i==r-1) or (i==j) or ():
+        if (j==0) or (i==r-1) or (i==j) :
             print("*", end=" ")
         else :
             print(" ",end=" ")
